fa9b7c14464fbdcdcc03ce09d6fc9d9e48163b24
Vasiliy <v.bilyuga@yandex.ru>
2025-05-11 12:28:58 +0300

    step 2

--- src/Differentiator.php
+++ src/Differentiator.php
@@ -9,15 +9,42 @@ use function GenDiff\FileService\getContents;
  * @throws \JsonException
  * @throws \Exception
  */
-function getDifference($file1, $file2): void
+function genDiff($pathToFile1, $pathToFile2): string
 {
-    $path1 = realpath($file1);
-    $path2 = realpath($file2);
-    $content1 = parseJson(getContents($path1));
-    $content2 = parseJson(getContents($path2));
-    var_dump($content1);
-    var_dump($content2);
+    $diff = [];
+
+    $content1 = getContent($pathToFile1);
+    $content2 = getContent($pathToFile2);
+    $intersect = array_intersect_assoc($content1, $content2);
+    $diff1 = array_diff_assoc($content1, $content2);
+    $diff2 = array_diff_assoc($content2, $content1);
+    $keys = array_merge(array_keys($intersect), array_keys($diff1), array_keys($diff2));
+    sort($keys, SORT_STRING);
+
+    foreach ($keys as $key) {
+        if (array_key_exists($key, $intersect)) {
+            $diff[$key] = $intersect[$key];
+            continue;
+        }
+
+        if (array_key_exists($key, $diff1)) {
+            $diff["-{$key}"] = $diff1[$key];
+        }
+
+        if (array_key_exists($key, $diff2)) {
+            $diff["+{$key}"] = $diff2[$key];
+        }
+    };
+
+    return json_encode($diff);
 }
 
-
-
+/**
+ * @throws \JsonException
+ * @throws \Exception
+ */
+function getContent($path): array
+{
+    $file = realpath($path);
+    return parseJson(getContents($file));
+}
--- src/Parser.php
+++ src/Parser.php
@@ -9,3 +9,11 @@ function parseJson(string $jsonString): array
 {
     return json_decode($jsonString, true, 512, JSON_THROW_ON_ERROR);
 }
+
+/**
+ * @throws \JsonException
+ */
+function getJson(array $jsonString): string
+{
+    return json_encode($jsonString, JSON_THROW_ON_ERROR);
+}
